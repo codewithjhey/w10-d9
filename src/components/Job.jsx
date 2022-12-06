1467bb85741dd0a8e501dbe4002629ae6fd92103
codewithjhey <114162135+codewithjhey@users.noreply.github.com>
2022-12-06 00:47:36 +0000

    button counter

--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -9,7 +9,7 @@ const Job = ({ data }) => {
   return (
     <Row
       className="mx-0 mt-3 p-3"
-      style={{ border: "1px solid #00000033", borderRadius: 4 }}
+      style={{ border: "2px solid #00000033", borderRadius: 4 }}
     >
       <Col xs={3}>
         <Link to={`/${data.company_name}`}>{data.company_name}</Link>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,12 @@
-import { Navbar, Nav } from "react-bootstrap"
+import { Navbar, Nav, Button } from "react-bootstrap"
 import { Link, useLocation } from "react-router-dom"
+import { useSelector } from "react-redux"
 
 const NavBar = ({ subtitle }) => {
   const location = useLocation()
+
+  const favLength = useSelector((state) => state.favourite.companies.length)
+
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Link to="/">
@@ -19,7 +23,10 @@ const NavBar = ({ subtitle }) => {
                   : "nav-link"
               }
             >
-              Favorites Jobs
+              <Button type="button" className="btn btn-warning">
+                Fav Jobs
+                <span className="badge badge-light ml-2">{favLength}</span>
+              </Button>
             </div>
           </Link>
         </Nav>
